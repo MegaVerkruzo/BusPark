6e641e907f0d7cce4f7fd2fc5e1609ddaafadc5d
MegaVerkruzo <robochat@mail.ru>
2021-07-12 23:48:15 +0300

    #1 AutoPark

--- src/components/Pages/AutoPark/Bus/Bus.js
+++ src/components/Pages/AutoPark/Bus/Bus.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import classes from './Bus.module.css'
+import bus from './../../../../utils/images/bus_park.png'
+import './../../../../App.css'
 
 const Bus = () => {
     return (
-        <div>
-            Bus
+        <div className={`${classes.content}`}>
+            <img src={bus}/>
+            <div>
+                Микроавтобусы рассчитаные на 23-24 места
+            </div>
+            <div>
+                <div>Стоимость:</div>
+                <div>
+                    <div>1 час:  1200 руб.</div>
+                    <div>1 км:   35 руб.</div>
+                </div>
+            </div>
+            <div >
+                <div className={`button ${classes.content__buttons}`}>
+                    Рассчитать
+                </div>
+                <div className={`button ${classes.content__buttons}`}>
+                    Уточнить цену
+                </div>
+            </div>
         </div>
     )
 }
