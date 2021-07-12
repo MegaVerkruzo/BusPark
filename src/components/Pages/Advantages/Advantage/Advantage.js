5cae280e76075170390b4e4d6aad2273752b95e7
MegaVerkruzo <robochat@mail.ru>
2021-07-13 00:22:15 +0300

    Доделал Advantage

--- src/components/Pages/Advantages/Advantage/Advantage.js
+++ src/components/Pages/Advantages/Advantage/Advantage.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import classes from './Advantage.module.css'
+import icon from './../../../../utils/images/icon_advantages.png'
+import './../../../../App.css'
 
-const Advantage = () => {
+const Advantage = (props) => {
     return (
-        <div>
-            Advantage
+        <div className={`${classes.content}`}>
+            <img className={`${classes.content__icon}`} src={icon}/>
+            <div className={`text_pred_title ${classes.content__title}`}>
+                {props.title}
+            </div>
+            <div className={`text_big_info ${classes.content__text}`}>
+                {props.explain}
+            </div>
         </div>
     )
 }
--- src/components/Pages/Advantages/Advantages.js
+++ src/components/Pages/Advantages/Advantages.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Advantage from "./Advantage/Advantage";
 
 const Advantages = () => {
     return (
         <div>
-            Advantages
+            <div>Наши преимущества</div>
+            <div>
+                <Advantage title='Лицензии на перевозку' explain='Все необходимые официальные разрешения на перевозки пассажиров'/>
+                <Advantage title='Опыт работы' explain='Огромный опыт работы и специальная квадификация наших сотрудников'/>
+                <Advantage title='Индивидуальный подход' explain='Индивидуальный подход к каждому нашему клиенту'/>
+            </div>
+            <div>
+                <Advantage title='Современная техника' explain='Современные транспортные средства различной вместительности'/>
+                <Advantage title='Страховка клиентов' explain='Современные транспортные средства различной вместительности'/>
+                <Advantage title='Низкая стоимость' explain='Современные  транспортные средства различной вместительности '/>
+            </div>
         </div>
     )
 }
