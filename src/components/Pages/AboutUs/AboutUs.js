fc0bfe6d5e88dd6ce419b8f0c75f267daf1490f0
MegaVerkruzo <robochat@mail.ru>
2021-07-13 08:33:08 +0300

    Доделал AboutUs

--- src/components/Pages/AboutUs/AboutUs.js
+++ src/components/Pages/AboutUs/AboutUs.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import classes from './AboutUs.module.css'
+import bus from './../../../utils/images/bus_park.png'
+import './../../../App.css'
 
 const AboutUs = () => {
     return (
-        <div>
-            AboutUs
+        <div className={`${classes.content}`}>
+            <img className={`${classes.content__image}`} src={bus}/>
+            <div className={`${classes.content__text}`}>
+                <div className={`text_title ${classes.content__text_element}`}>
+                    Кто мы?
+                </div>
+                <div className={`text_info ${classes.content__text_element}`}>
+                    В своём стремлении повысить качество жизни, они забывают, что высокое качество позиционных
+                    исследований предоставляет широкие возможности для соответствующих условий активизации. Банальные,
+                    но неопровержимые выводы, а также диаграммы связей лишь добавляют фракционных разногласий и смешаны
+                    с не уникальными данными до степени совершенной неузнаваемости, из-за чего возрастает их статус
+                    бесполезности
+                </div>
+                <div className={`text_info ${classes.content__text_element}`}>
+                    В своём стремлении повысить качество жизни, они забывают, что высокое качество позиционных
+                    исследований предоставляет широкие возможности для соответствующих условий активизации. Банальные,
+                    но неопровержимые выводы, а также диаграммы связей лишь добавляют фракционных разногласий и смешаны
+                    с не уникальными данными до степени совершенной неузнаваемости, из-за чего возрастает их статус
+                    бесполезности
+                </div>
+                <div className={`button ${classes.content__button}`}>
+                     Рассчитать
+                </div>
+            </div>
         </div>
     )
 }
